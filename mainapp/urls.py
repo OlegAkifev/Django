fc6b3c72c4cc226b274122fae53a61b053c5cb0b
OlegAkifev <102689292+OlegAkifev@users.noreply.github.com>
2023-09-28 20:27:25 +0300

    Lesson 4
    * lesson_4 News Model
    
    * lesson_4 Models
    
    * lesson_4 Models
    
    * lesson_4 Migrations
    
    * lesson_4 Fixtures
    
    * lesson_4 Data migration
    
    * lesson_4 display Model's data
    
    * add models and News/Courses views

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic.base import RedirectView
+
 from mainapp import views
 from mainapp.apps import MainappConfig
 
@@ -8,9 +8,13 @@ app_name = MainappConfig.name
 urlpatterns = [
     path("", views.MainPageView.as_view(), name="main_page"),
     path("news/", views.NewsPageView.as_view(), name="news"),
-    path("news/<int:page>/", views.NewsWithPaginatorView.as_view(), name="news_paginator"),
-    path("news/<str:title>/", RedirectView.as_view(url='https://yandex.ru/search/?text=%(title)s&lr=213'), name="search"),
-    path("courses/", views.CoursesPageView.as_view(), name="courses"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path(
+        "courses/<int:pk>/",
+        views.CoursesDetailView.as_view(),
+        name="courses_detail",
+    ),
     path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
     path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
     path("login/", views.LoginPageView.as_view(), name="login"),
