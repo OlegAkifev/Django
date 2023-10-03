134fff3b5d8dbcafbf0831e0466203f8eca57e30
OlegAkifev <102689292+OlegAkifev@users.noreply.github.com>
2023-10-04 01:34:49 +0300

    Lesson 5 (#6)
     GitHub auth

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "markdownify.apps.MarkdownifyConfig",
+    "social_django",
     "mainapp",
     "authapp",
 
@@ -74,6 +75,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "mainapp.context_processors.example.simple_context_processor",
+                "social_django.context_processors.backends",
+                "social_django.context_processors.login_redirect",
+
             ],
         },
     },
@@ -113,6 +117,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 AUTH_USER_MODEL = "authapp.CustomUser"
 
+
+AUTHENTICATION_BACKENDS = (
+    "social_core.backends.github.GithubOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
+)
+
 LOGIN_REDIRECT_URL = "mainapp:main_page"
 LOGOUT_REDIRECT_URL = "mainapp:main_page"
 
@@ -150,3 +160,5 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
 
+SOCIAL_AUTH_GITHUB_KEY = "7ca8e7a61139d6115aed"
+SOCIAL_AUTH_GITHUB_SECRET = "07b5a2ff2f4a1e207b8ea7a64469054c238abc11"
